452676275c60c2b8d275231d7caffba1f6c585b2
Alexis.Avril <alexisavril@sfr.fr>
2026-03-13 20:47:29 +0100

    first PostFeed work

---
+++ components/ui/PostFeed.tsx
@@ -0,0 +1,22 @@
+import { PostCard } from "./PostCard"
+
+interface PostFeedProps {
+  posts: {
+    id: string
+    author: string
+    date: string
+    content: string
+    mediaUrl?: string
+    mediaType?: "image" | "video"
+  }[]
+}
+
+export function PostFeed({ posts }: PostFeedProps) {
+  return (
+    <div className="mt-4">
+      {posts.map(post => (
+        <PostCard key={post.id} {...post} />
+      ))}
+    </div>
+  )
+}
